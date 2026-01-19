8d49c992e918f4cdfcb2cf3a64bf2b8bec17ff38
Arnav Chennamaneni <chennamaneniarnav@gmail.com>
2026-01-19 17:47:45 -0500

    Set up state to be in GCS bucket (#2)

--- main.tf
+++ main.tf
@@ -1,2 +1,7 @@
 # main.tf - defines resources in our cloud (GCP)
 # Placeholder file to preserve project structure
+
+resource "google_storage_bucket" "bucket_container" {
+  name     = var.bucket_name
+  location = "US"
+}
--- terraform.tf
+++ terraform.tf
@@ -1,10 +1,17 @@
 # General terraform config shit
+# To connect to remote state, run the command: terraform init \
+#-backend-config="bucket={bucket_name}" \
+#-reconfigure 
+
 terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
       version = "4.57.0"
     }
+  }
+  backend "gcs" {
+    prefix = "terraform/state"
   }
 }
 provider "google" {
